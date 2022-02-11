7e37c186e28ef0db41209d48e5207f7e37f21295
nikhilvenkat-999 <95400869+nikhilvenkat-999@users.noreply.github.com>
2022-02-11 18:31:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "eks" 
+    default     =  "LockId " 
 }
 
 variable "instance_type" { 
