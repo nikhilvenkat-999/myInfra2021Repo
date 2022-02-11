bd7caf9b60c6f8cdaa538c5aefca7b557117d353
nikhilvenkat-999 <95400869+nikhilvenkat-999@users.noreply.github.com>
2022-02-11 17:19:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "aws_region" {
        default     = "us-east-1" 
 }
 
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "eks" 
+}
+
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
